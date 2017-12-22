a985ec67249e0ddcdee2ef996d7e2208ee7f94af
lizhen <1627979701@qq.com>
2017-12-22 11:01:35 +0800

    添加Controller

--- src/main/java/com/offer/controller/UserController.java
+++ src/main/java/com/offer/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.offer.controller;
 
+import org.springframework.stereotype.Controller;
+
 /**
  * @Author lizhen [v_zhennli@tencent.com]
  * @Date 2017/12/22 10:41
  */
+@Controller
 public class UserController {
 }
